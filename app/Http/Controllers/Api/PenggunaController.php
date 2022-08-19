e0f1689e1ea82458cca9802f540751fadc0c8307
NazranH <nazranh18@gmail.com>
2022-08-19 13:15:26 +0700

    user

---
+++ app/Http/Controllers/Api/PenggunaController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Pengguna;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\PenggunaResource;
+use Illuminate\Support\Facades\Validator;
+
+class PenggunaController extends Controller
+{
+    public function index()
+    {
+        $pengguna = Pengguna::latest()->paginate(5);
+
+        return new PenggunaResource(true, 'List Data Pengguna', $pengguna);
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama_lengkap'     => 'required',
+            'email'     => 'required',
+            'tgl_lahir'   => 'required',
+            'NIK'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create post
+        $pengguna = Pengguna::create([
+            'nama_lengkap'     => $request->nama_lengkap,
+            'email'     => $request->email,
+            'tgl_lahir'   => $request->tgl_lahir,
+            'NIK'   => $request->NIK,
+        ]);
+
+        //return response
+        return new PenggunaResource(true, 'Data Post Berhasil Ditambahkan!', $pengguna);
+    }
+
+    public function show(Pengguna $pengguna)
+    {
+        //return single post as a resource
+        return new PenggunaResource(true, 'Data Post Ditemukan!', $pengguna);
+    }
+
+    public function update(Request $request, Pengguna $pengguna)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama_lengkap'     => 'required',
+            'email'   => 'required',
+            'tgl_lahir'   => 'required',
+            'NIK'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //return response
+        return new PenggunaResource(true, 'Data Post Berhasil Diubah!', $pengguna);
+    }
+
+    public function destroy(Pengguna $pengguna)
+    {
+
+        //delete post
+        $pengguna->delete();
+
+        //return response
+        return new PenggunaResource(true, 'Data Post Berhasil Dihapus!', null);
+    }
+}
